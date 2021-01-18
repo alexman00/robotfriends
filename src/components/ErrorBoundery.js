bbb5707cc67ee3b1fa376821f13a76c9756d2e18
Alexman <alexandru.mandache.work@gmail.com>
2021-01-18 20:24:55 +0200

    final changes to the robot

---
+++ src/components/ErrorBoundery.js
@@ -0,0 +1,24 @@
+// import React, {Component} from 'react';
+
+// class ErrorBoundery extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             hasError: false
+//         }
+//     }
+
+//     render(){
+//         if (this.state.hasError) {
+//             return <h1>opps. That is not good </h1>
+//         }
+
+//         componentDidCatch(error, info);{
+//             this.setState({ hasError: true })
+
+//         }
+//         return this.props.children;
+//     }
+// }
+
+// export default ErrorBoundery;
